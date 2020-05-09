2b2d38c119a174683cd168ebf54c0935cb9ecf62
alew <ale.w.es@sync.money>
2020-05-09 21:46:43 +0200

    configure auth0 application

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'alew-dev.eu.auth0.com',            // Auth0 domain
+  clientId: 'g2lF5pYQHG14xmV42ebltzRooi7gFuSn',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
